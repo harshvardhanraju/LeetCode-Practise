d66a474cc5bea80750c33c2c40a2b6a5d260b185
Harshvardhan Raju <harshvardhanraju@users.noreply.github.com>
2025-05-13 14:03:09 +0530

    Time: 78 ms (97.36%), Space: 29.1 MB (44.2%) - LeetHub

---
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
@@ -0,0 +1,35 @@
+import heapq
+from collections import defaultdict
+
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        
+
+        # grid/graph matrix needs to be created
+        graph = defaultdict(list)
+        # for prob, (u, v)  in enumerate(edges):
+        #     graph[u].append((v, prob[i]))
+        #     graph[v].append((u, prob[i]))
+        for (u, v), prob in zip(edges, succProb):
+            graph[u].append((v, prob))
+            graph[v].append((u, prob))  # undirected
+
+        dist = [0.0] * (n+1)
+        dist[start_node] = 1.0
+        max_heap = [(-1.0, start_node)] #added startng node with 0 weight
+
+        while max_heap:
+            curr_prob, u = heapq.heappop(max_heap)
+            curr_prob = - curr_prob #going back to actual prob values as we negated them earlier
+            if u == end_node:
+                return curr_prob
+
+            # if new_dist < dist[u]: #skip and dont update
+            #     continue
+            for v, prob in graph[u]:
+                if dist[v] < curr_prob * prob: #i.e older path was longer, then update to new
+                    dist[v] = curr_prob * prob
+                    heapq.heappush(max_heap, (-dist[v], v))
+        
+        
+        return 0
