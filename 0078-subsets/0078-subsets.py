430da0093c50140b4b0a27aebe6c1fc052d3369b
Harshvardhan Raju <harshvardhanraju@users.noreply.github.com>
2025-05-26 11:54:36 +0530

    Time: 0 ms (100%), Space: 17.7 MB (90.63%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,23 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [] 
+        subset = []
+
+        def recurse_set(nums, i, subset, res):
+            if i == len(nums):
+                res.append(list(subset))
+                return
+            
+            #pick ith element
+            subset.append(nums[i])
+            # print(subset)
+            #recurse
+            recurse_set(nums, i + 1, subset, res)
+            #not pick ith element
+            subset.pop()
+            #recurse
+            recurse_set(nums, i + 1, subset, res)
+        
+        recurse_set(nums, 0, subset, res)
+        return(res)
+
