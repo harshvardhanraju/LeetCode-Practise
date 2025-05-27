0a44d798ef92f8ade95610f270d0bc59cda799f3
Harshvardhan Raju <harshvardhanraju@users.noreply.github.com>
2025-05-27 14:26:36 +0530

    Time: 2 ms (39.25%), Space: 18.1 MB (33.69%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,23 +1,23 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        # lets try recursion and backtracking
-        ans = []
-        
-        def recurse(nums, idx, ans):
-            #end case
-            if idx == len(nums):
-                ans.append(nums[:])
+        result = []
+
+        def backtrack(state, used):
+            if len(state) == len(nums):
+                result.append(list(state))
                 return
-            
-            for i in range(idx, len(nums)):
-                nums[idx], nums[i] = nums[i], nums[idx]
-                recurse(nums, idx+1, ans)
-                nums[idx], nums[i] = nums[i], nums[idx]
 
+            for i in range(len(nums)):
+                if used[i] == True:
+                    continue # skip this number as its used already
+                else:
+                    used[i] = True
+                state.append(nums[i])
+                backtrack(state, used)
+                state.pop()
+                used[i] = False
+       
+        backtrack([], [False] * len(nums))
 
-     
-        recurse(nums, 0, ans)
-        return(ans)
-
-        
+        return result
         
